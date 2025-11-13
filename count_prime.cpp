cc75dd8ac15f8e8935ec58dca82c95c2905111d7
ambatroncpu <67991049@kmitl.ac.th>
2025-11-13 10:17:48 +0700

    Create count_prime.cpp

---
+++ count_prime.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+  int end,count=0,num_track=7;
+  vector<int> store_prime;
+  store_prime.push_back(2);
+  store_prime.push_back(3);
+  cin>>end;
+  if (end>=3) {
+    count = 2;
+  }else{
+    count = 1;
+  }
+  while(num_track>end) {
+    int determine=0;
+    for(int i=0;i<count;i++) {
+      if(num_track%sstore_prime[i]==0) {
+        determine = num_track;
+        break;
+      }
+      num_track++;
+      if(determine!=0) {
+        determine = 0;
+        continue;
+      }
+      count++;
+      store_prime.push_back(determine);
+    }
+  }
+  cout<<count;
+  return 0;
+}
