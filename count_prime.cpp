4bf3ced3a864d6929af4262a92e306cba7f302f4
ambatroncpu <67991049@kmitl.ac.th>
2025-11-14 10:35:09 +0700

    Update count_prime.cpp

--- count_prime.cpp
+++ count_prime.cpp
@@ -1,31 +1,48 @@
 #include<iostream>
 #include<vector>
+#include<cmath>
 using namespace std;
 
 int main() {
-  int end,count=0,num_track=7;
-  vector<int> store_prime;
-  store_prime.push_back(2);
-  store_prime.push_back(3);
+  int end,count=0,pos=1;
+  vector<int> store;
+  store.push_back(2);
+  store.push_back(3);
   cin>>end;
-  if (end>=3) {
+  if(end>=3){
     count = 2;
-  }else{
+  }else if(end==2) {
     count = 1;
+  }else{
+    count = 0;
   }
-  while(num_track<=end) {
-    int determine=0;
-    for(int i=0;i<count;i++) {
-      if(num_track%store_prime[i]==0) {
-        determine = 1;
+  while(end>=6*pos-1) {
+    int deter1=6*pos-1,deter2=6*pos+1;
+    for (int i=0;i<count;i++) {
+      if((6*pos-1) % store[i] == 0) {
+        deter1 = 1;
+        break;
+      }else if (store[i]>sqrt(6*pos-1)) {
         break;
       }
     }
-      if(determine==0) {
-        count++;
-        store_prime.push_back(num_track);
+    for (int i=0;i<count;i++) {
+      if((6*pos+1) % store[i] == 0 || 6*pos+1>end) {
+        deter2 = 1;
+        break;
+      }else if (store[i]>sqrt(6*pos+1)) {
+        break;
       }
-      num_track++;
+    }
+    if (deter1!=1) {
+      count++;
+      store.push_back(deter1);
+    }
+    if (deter2!=1) {
+      count++;
+      store.push_back(deter2);
+    }
+    pos++;
   }
   cout<<count;
   return 0;
